297ec929209020616deed3ac6a91427556aeda8a
Matt Ricketson <mricketson@mac.com>
2014-05-30 14:20:59 -0400

    Refactor names and complete documentation

--- Classes/PHYExtendedAppDelegate.h
+++ Classes/PHYExtendedAppDelegate.h
@@ -1,30 +1,36 @@
+// PHYExtendedAppDelegate.h
 //
-//  PHStandardAppDelegate.h
-//  Rally
+// Copyright (c) 2014 Phyre Inc.
 //
-//  Created by Matt Ricketson on 11/25/13.
-//  Copyright (c) 2013 Phyre. All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import <UIKit/UIKit.h>
-
-extern NSString * const PHYAppDelegateDidOpenURLNotification;
-extern NSString * const PHYAppDelegateDidOpenURLSourceApplicationKey;
-extern NSString * const PHYAppDelegateDidOpenURLAnnotationKey;
-extern NSString * const PHYAppDelegateDidOpenURLURLKey;
-extern NSString * const PHYAppDelegateDidOpenURLQueryDataKey;
-
-extern BOOL phy_isRunningTests(void) __attribute__((const));
 
 typedef void(^PHYRemoteNotificationRegistrationSuccesshandler)(NSData *deviceToken);
 typedef void(^PHYRemoteNotificationRegistrationFailurehandler)(NSError *error);
 
 
-
-#pragma mark -
-
 /**
- `PHAppDelegate` is an app delegate superclass designed to improve the convenience of common app delegate tasks such as remote notification registration.
+ `PHYExtendedAppDelegate` is an app delegate superclass designed to improve the convenience of common app delegate tasks such as 
+ remote notification registration and handling open URLs.
+ 
+ To use `PHYExtendedAppDelegate`, simply declare it as the superclass of your project's existing `UIApplicationDelegate` instance.
  */
 @interface PHYExtendedAppDelegate : UIResponder <UIApplicationDelegate>
 
@@ -36,9 +42,9 @@ typedef void(^PHYRemoteNotificationRegistrationFailurehandler)(NSError *error);
 + (instancetype)sharedDelegate;
 
 
-///--------------------------
-/// @name App Characteristics
-///--------------------------
+///--------------------------------
+/// @name User Interface Management
+///--------------------------------
 
 /// The application window.
 @property (nonatomic, strong) UIWindow *window;
@@ -48,24 +54,83 @@ typedef void(^PHYRemoteNotificationRegistrationFailurehandler)(NSError *error);
 /// @name Handling Remote Notifications
 ///------------------------------------
 
+/**
+ Register to receive notifications of the specified types from a provider via Apple Push Service.
+ 
+ @param success A block that is executed if registration is successful. Returns the APNs device token.
+ @param failure A block that is executed if registration is unsuccessful. Returns an error explaining the issue.
+ 
+ @see -registerForRemoteNotificationTypes:success:failure:
+ */
 - (void)registerForRemoteNotifications:(PHYRemoteNotificationRegistrationSuccesshandler)success
 							   failure:(PHYRemoteNotificationRegistrationFailurehandler)failure;
 
+/**
+ Register to receive notifications of the specified types from a provider via Apple Push Service.
+ 
+ @param types The specific remote notification types to register for.
+ @param success A block that is executed if registration is successful. Returns the APNs device token.
+ @param failure A block that is executed if registration is unsuccessful. Returns an error explaining the issue.
+ */
 - (void)registerForRemoteNotificationTypes:(UIRemoteNotificationType)types
 								   success:(PHYRemoteNotificationRegistrationSuccesshandler)success
 								   failure:(PHYRemoteNotificationRegistrationFailurehandler)failure;
 
-
-///-------------------------
-/// @name Handling Open URLs
-///-------------------------
-
-- (NSDictionary *)dictionaryForParsedQueryString:(NSString *)query;
-
 @end
 
 
+#pragma mark -
 
-#pragma mark - UIApplication Extensions
+///--------------------
+/// @name Notifications
+///--------------------
 
-extern NSString * NSStringFromUIApplicationState(UIApplicationState appState);
+/// Posted when the app delegate receives a URL via -application:openURL:sourceApplication:annotation:
+extern NSString * const PHYExtendedAppDelegateDidOpenURLNotification;
+
+/// The corresponding value is the `sourceApplication` provided by -application:openURL:sourceApplication:annotation:
+extern NSString * const PHYExtendedAppDelegateURLSourceApplicationKey;
+
+/// The corresponding value is the `annotation` provided by -application:openURL:sourceApplication:annotation:
+extern NSString * const PHYExtendedAppDelegateURLAnnotationKey;
+
+/// The corresponding value is the `url` provided by -application:openURL:sourceApplication:annotation:
+extern NSString * const PHYExtendedAppDelegateURLKey;
+
+/// The corresponding value is the dictionary representation of `url` query provided by -application:openURL:sourceApplication:annotation:
+extern NSString * const PHYExtendedAppDelegateURLQueryDataKey;
+
+
+///------------------------
+/// @name Utility Functions
+///------------------------
+
+/**
+ Checks if the current run of the application is for unit tests.
+ 
+ @discussion
+ This function is useful for conditionally setting up an application depending on whether or not unit tests are 
+ running. For example, it is sometimes not desirable to initialize an app's normal user interface when running 
+ unit tests.
+ 
+ @return YES if the app launched for the purpose of running unit tests, NO if otherwise.
+ */
+BOOL PHYApplicationIsRunningTests(void) __attribute__((const));
+
+/**
+ Parses a query string into a dictionary of key/value pairs.
+ 
+ @param url A URL with a query string.
+ 
+ @return A dictionary containing the key/value pairs of the query string. If `[URL query]` returns `nil`, then an empty dictionary is returned.
+ */
+NSDictionary * PHYURLQueryDictionaryRepresentation(NSURL *url);
+
+/**
+ Provides a string representation of a `UIApplicationState` enum value. Useful for debugging purposes.
+ 
+ @param appState A `UIApplicationState` enum value.
+ 
+ @return An `NSString` representing the `UIApplicationState` enum value.
+ */
+NSString * PHYStringFromUIApplicationState(UIApplicationState appState);
--- Classes/PHYExtendedAppDelegate.m
+++ Classes/PHYExtendedAppDelegate.m
@@ -1,35 +1,31 @@
+// PHYExtendedAppDelegate.m
 //
-//  PHStandardAppDelegate.m
-//  Rally
+// Copyright (c) 2014 Phyre Inc.
 //
-//  Created by Matt Ricketson on 11/25/13.
-//  Copyright (c) 2013 Phyre. All rights reserved.
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
 //
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 #import "PHYExtendedAppDelegate.h"
 
-NSString * const PHYAppDelegateDidOpenURLNotification			= @"PHYAppDelegateDidOpenURLNotification";
-NSString * const PHYAppDelegateDidOpenURLSourceApplicationKey	= @"PHYAppDelegateDidOpenURLSourceApplicationKey";
-NSString * const PHYAppDelegateDidOpenURLAnnotationKey			= @"PHYAppDelegateDidOpenURLAnnotationKey";
-NSString * const PHYAppDelegateDidOpenURLURLKey					= @"PHYAppDelegateDidOpenURLURLKey";
-NSString * const PHYAppDelegateDidOpenURLQueryDataKey			= @"PHYAppDelegateDidOpenURLQueryDataKey";
-
-BOOL phy_isRunningTests(void)
-{
-	NSDictionary *environment = [[NSProcessInfo processInfo] environment];
-    NSString *injectBundle = environment[@"XCInjectBundle"];
-	NSString *injectBundlePathExtension = [injectBundle pathExtension];
-    return [injectBundlePathExtension isEqualToString:@"xctest"];
-}
-
-
 @interface PHYExtendedAppDelegate ()
-
 @property (nonatomic, copy) PHYRemoteNotificationRegistrationSuccesshandler remoteNotificationRegistrationSuccessHandler;
 @property (nonatomic, copy) PHYRemoteNotificationRegistrationFailurehandler remoteNotificationRegistrationFailureHandler;
-
 @end
-
 
 @implementation PHYExtendedAppDelegate
 
@@ -87,21 +83,44 @@ BOOL phy_isRunningTests(void)
 - (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation
 {
 	NSDictionary *userInfo = @{
-							   PHYAppDelegateDidOpenURLSourceApplicationKey:	sourceApplication,
-							   PHYAppDelegateDidOpenURLAnnotationKey:			annotation,
-							   PHYAppDelegateDidOpenURLURLKey:					url,
-							   PHYAppDelegateDidOpenURLQueryDataKey:			[self dictionaryForParsedQueryString:[url query]]
+							   PHYExtendedAppDelegateURLSourceApplicationKey:	sourceApplication,
+							   PHYExtendedAppDelegateURLAnnotationKey:          annotation,
+							   PHYExtendedAppDelegateURLKey:                    url,
+							   PHYExtendedAppDelegateURLQueryDataKey:           PHYURLQueryDictionaryRepresentation(url)
 							   };
 	
-	[[NSNotificationCenter defaultCenter] postNotificationName:PHYAppDelegateDidOpenURLNotification object:self userInfo:userInfo];
+	[[NSNotificationCenter defaultCenter] postNotificationName:PHYExtendedAppDelegateDidOpenURLNotification object:application userInfo:userInfo];
 	
 	return YES;
 }
 
-- (NSDictionary *)dictionaryForParsedQueryString:(NSString *)query
+@end
+
+
+
+#pragma mark - Notifications
+
+NSString * const PHYExtendedAppDelegateDidOpenURLNotification   = @"PHYExtendedAppDelegateDidOpenURLNotification";
+NSString * const PHYExtendedAppDelegateURLSourceApplicationKey  = @"PHYExtendedAppDelegateURLSourceApplicationKey";
+NSString * const PHYExtendedAppDelegateURLAnnotationKey         = @"PHYExtendedAppDelegateURLAnnotationKey";
+NSString * const PHYExtendedAppDelegateURLKey                   = @"PHYExtendedAppDelegateURLKey";
+NSString * const PHYExtendedAppDelegateURLQueryDataKey          = @"PHYExtendedAppDelegateURLQueryDataKey";
+
+
+#pragma mark - Utility Functions
+
+BOOL PHYApplicationIsRunningTests(void)
+{
+    NSDictionary *environment = [[NSProcessInfo processInfo] environment];
+    NSString *injectBundle = environment[@"XCInjectBundle"];
+	NSString *injectBundlePathExtension = [injectBundle pathExtension];
+    return [injectBundlePathExtension isEqualToString:@"xctest"];
+}
+
+NSDictionary * PHYURLQueryDictionaryRepresentation(NSURL *url)
 {
     NSMutableDictionary *queryData = [NSMutableDictionary dictionary];
-    NSArray *pairs = [query componentsSeparatedByString:@"&"];
+    NSArray *pairs = [[url query] componentsSeparatedByString:@"&"];
     
     for (NSString *pair in pairs) {
         NSArray *elements = [pair componentsSeparatedByString:@"="];
@@ -110,20 +129,14 @@ BOOL phy_isRunningTests(void)
         queryData[key] = val;
     }
 	
-    return queryData;
+    return [queryData copy];
 }
 
-@end
-
-
-
-#pragma mark - UIApplication Extensions
-
-NSString * NSStringFromUIApplicationState(UIApplicationState appState)
+NSString * PHYStringFromUIApplicationState(UIApplicationState appState)
 {
 	switch (appState) {
-		case UIApplicationStateActive:		return @"UIApplicationStateActive";
-		case UIApplicationStateInactive:	return @"UIApplicationStateInactive";
-		case UIApplicationStateBackground:	return @"UIApplicationStateBackground";
+		case UIApplicationStateActive:      return @"UIApplicationStateActive";
+		case UIApplicationStateInactive:    return @"UIApplicationStateInactive";
+		case UIApplicationStateBackground:  return @"UIApplicationStateBackground";
 	}
 }
